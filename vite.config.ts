6138adf0c09fce9861a7ecdc65cd2eaf1503deec
Patryk Jezierski <wacool00@gmail.com>
2022-03-11 07:41:09 +0100

    disabled eslint plugin cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import path from 'path';
-
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import WindiCSS from 'vite-plugin-windicss';
+import path from 'path';
+import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS(), eslintPlugin()],
+  plugins: [vue(), WindiCSS(), eslintPlugin({ cache: false })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
